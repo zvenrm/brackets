2aea7b2660428ba4def64a85d0b720e8f021eb81
zvenrm <zvennnnnn@gmail.com>
2022-01-19 13:18:14 +0300

    feat: complete brackets task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,34 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let mas = [];
+  let open = [];
+  let pairs = {};
+  bracketsConfig.forEach(element => {
+    open.push(element[0]);
+    pairs[element[1]] = element[0];
+  });
+  for (let i = 0; i < str.length; i++){
+    if (open.includes(str[i]) && Object.keys(pairs).includes(str[i])){
+      if (mas[mas.length - 1] === str[i]){
+        mas.pop();
+      }
+      else {
+        mas.push(str[i])
+      }
+    }
+    else if (open.includes(str[i])){
+      mas.push(str[i]);
+    }
+    else {
+      if (mas.length === 0){
+        return false;
+      }
+      if (pairs[str[i]] === mas[mas.length - 1]){
+        mas.pop();
+      }
+      else {
+        return false;
+      }
+    }
+  }
+  return mas.length === 0;
 }
